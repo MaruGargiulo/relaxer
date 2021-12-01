14a10a71f8f233c29c9b5b6b1b5b91d475208c44
Maru Gargiulo <38535964+MaruGargiulo@users.noreply.github.com>
2021-12-01 02:03:07 -0300

    upload project

---
+++ app.js
@@ -0,0 +1,29 @@
+window.addEventListener('load', () => {
+
+const container = document.querySelector('.container')
+const text = document.querySelector('#text')
+
+const totalTime = 7500
+const breatheTime = (totalTime / 5) * 2
+const holdTime = (totalTime / 5)
+
+breatheAnimation()
+
+function breatheAnimation() {
+    text.innerHTML = 'Breathe in'
+    container.className = 'container grow'
+    
+    setTimeout(() => {
+        text.innerHTML = 'Hold'
+        
+        setTimeout(() => {
+            text.innerHTML = 'Breate out'
+            container.className = 'container shrink'
+        }, holdTime)
+        
+    }, breatheTime)
+}
+
+setInterval(breatheAnimation, totalTime)
+
+})
